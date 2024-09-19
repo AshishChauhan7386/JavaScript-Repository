e93c4c4855d1f65d26b97af15d97a8e5887647dc
AshishChauhan7386 <chauhanashish7386@gmail.com>
2024-09-19 14:03:59 +0530

    Promises Practice

--- Promises.js
+++ Promises.js
@@ -1,1 +1,33 @@
-console.log("Ashish Chauhan")
+const fetchData = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const success = true;
+
+    if (success) {
+      resolve("Fetch Data!");
+    } else {
+      reject("Error!");
+    }
+  }, 2000);
+});
+
+fetchData
+  .then((result) => {
+    console.log(result);
+    return isSuccess(result);
+  })
+  .then((FinalData) => {
+    console.log(FinalData);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+const isSuccess = (msg) =>
+  new Promise((resolve, reject) => {
+    const isSuccess = true;
+    if (isSuccess) {
+      resolve(`${msg} - Ashish Chauhan`);
+    } else {
+      reject("Error");
+    }
+  });
